a77a5357ba133507753e110f352b418df615de80
Marius <41506006+xXMariusXx@users.noreply.github.com>
2019-03-23 13:50:47 +0100

    .

--- models/quiz.js
+++ models/quiz.js
@@ -27,5 +27,5 @@ function bestimmeFrage() {
     return fragen;
 }
 
-module.exports.bestimmeFrage = bestimmeFrage;
+module.exports.bestimmeFrage = bestimmeFrage();
 
--- routes/router.js
+++ routes/router.js
@@ -7,6 +7,7 @@ router.get('/',function(req,res,next){
     res.render("dashboard");
 });
 
+//quiz
 router.get('/fragen',function(req,res,next){
     res.render("quiz",{
         quizarr: quiz.bestimmeFrage
