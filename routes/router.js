5d8e51852f0bb0f9584b72c89ec26b8f89f4f34c
Marius <41506006+xXMariusXx@users.noreply.github.com>
2019-03-23 12:14:47 +0100

    .

--- routes/router.js
+++ routes/router.js
@@ -1,33 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const rooms = require("./../models/rooms");
-
 
 //Dashboard
 router.get('/',function(req,res,next){
     res.render("dashboard");
-});
-
-//Raumliste
-router.get("/raumliste",function(req,res,next){
-   res.render("raumliste",{
-    raeume: rooms.raumArray
-    });
-});
-
-//Raumdetails
-router.get("/raumdetails",function(req,res,next){
-    res.render("raumdetails");
-});
-
-//Buchungdetails
-router.get("/buchungdetails",function(req,res,next){
-    res.render("buchungdetails");
-});
-
-//neueBuchung
-router.get("/neuebuchung",function(req,res,next){
-    res.render("neuebuchung");
 });
 
 //404
